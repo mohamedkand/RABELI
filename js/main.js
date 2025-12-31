3837a6b670d6d8e411bb0494565d12754be2d8c9
Simo <skandri30@gmail.com>
2025-12-31 12:14:29 +0100

    Add full RABELI website

---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.log("RABELI website loaded successfully");
+
+document.getElementById('easterEgg').addEventListener('click', () => {
+  alert("RABELI sees you. And she’s flattered. 😏");
+});
